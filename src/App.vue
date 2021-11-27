8ab841218364b5a2eaf3be62e5eb22a258d0cea2
unknown <tinb93273@student.ctu.edu.vn>
2021-11-27 19:58:15 +0700

    Create Todo-List

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <TodosList />
+      <TodosListV3 />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodosList from "./components/TodosList";
+import TodosListV3 from "./components/TodosListV3";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodosList,
+    TodosListV3,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +24,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-align: center;
 }
 </style>
